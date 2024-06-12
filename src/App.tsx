ac331e48b7d0e677912809517e81229765a545be
hunganh010805 <teugiatre123@gmail.com>
2024-06-12 07:13:24 +0700

    ss58

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import axios from 'axios'
+import ListInforStudent from './components/b1,b2,b3,b4,b5,b6/ListInforStudent'
+export default function App() {
+  return (
+    <div>
+       <ListInforStudent></ListInforStudent>
+     
+    </div>
+  )
+}
